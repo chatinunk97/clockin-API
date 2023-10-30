78a11f63c644e8b1c5af3f1ab330f70bcc2ab2e4
sakaorat36 <sakaoratploy@gmail.com>
2023-10-30 18:32:10 +0700

    feature/leave

--- src/server.js
+++ src/server.js
@@ -3,24 +3,22 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
 
 //Routes
+const leaveRoute = require("./routes/leave-route");
 
 //Middlewares
-const requestLimitMiddleware = require('./middleware/defaultMiddleware/requestLimit')
+const requestLimitMiddleware = require("./middleware/defaultMiddleware/requestLimit");
 
 //Default Middleware
-app.use(cors())
-app.use(requestLimitMiddleware)
+app.use(cors());
+app.use(requestLimitMiddleware);
 app.use(morgan("dev"));
 app.use(express.json());
 //////////
 
-app.use('/' , (req,res,next)=>{
-  res.json({message : "Connected ! "})
-})
-
+app.use("/leave", leaveRoute);
 
 app.listen(PORT || 8080, () => {
   console.log(" ############ Server is running on PORT ", +PORT || 8080);
