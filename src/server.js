46d344da59b2df1e8c70230870c216e69cfabad3
Tar <Tarta12296@gmail.com>
2023-10-30 17:34:23 +0700

    USER: create, delete

--- src/server.js
+++ src/server.js
@@ -1,29 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT;
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
 
 //Routes
-const authAdminRoute = require('../src/routes/authAdmin-route');
+const authAdminRoute = require("../src/routes/authAdmin-route");
+const authUserRoute = require("./routes/authUser-route");
 
 //Middlewares
-const requestLimitMiddleware = require('./middleware/defaultMiddleware/requestLimit');
-const errorMiddleware = require('../src/middleware/defaultMiddleware/error');
-const notFoundMiddleware = require('../src/middleware/defaultMiddleware/not-found');
+const requestLimitMiddleware = require("./middleware/defaultMiddleware/requestLimit");
+const errorMiddleware = require("../src/middleware/defaultMiddleware/error");
+const notFoundMiddleware = require("../src/middleware/defaultMiddleware/not-found");
 //Default Middleware
 app.use(cors());
 app.use(requestLimitMiddleware);
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 //////////
 
-app.use('/admin', authAdminRoute);
+app.use("/admin", authAdminRoute);
+app.use("/users", authUserRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
 app.listen(PORT || 8080, () => {
-  console.log(' ############ Server is running on PORT ', +PORT || 8080);
+  console.log(" ############ Server is running on PORT ", +PORT || 8080);
 });
