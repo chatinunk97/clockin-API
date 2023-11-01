f50a1fb49c974e1041b7ade14a6acd851e86882e
Prapawin <maiprapawin@gmail.com>
2023-11-01 16:20:45 +0700

    added: getMe and delete user.password on authenticate.js

--- src/middleware/authenticate/authenticate.js
+++ src/middleware/authenticate/authenticate.js
@@ -1,24 +1,25 @@
-const createError = require('../../utils/create-error');
-const jwt = require('jsonwebtoken');
-const prisma = require('../../models/prisma');
+const createError = require("../../utils/create-error");
+const jwt = require("jsonwebtoken");
+const prisma = require("../../models/prisma");
 
 module.exports = async (req, res, next) => {
   try {
     const authorization = req.headers.authorization;
-    if (!authorization || !authorization.startsWith('Bearer '))
-      return next(createError('Unauthenticated', 401));
+    if (!authorization || !authorization.startsWith("Bearer "))
+      return next(createError("Unauthenticated", 401));
 
-    const token = authorization.split(' ')[1];
+    const token = authorization.split(" ")[1];
     const payload = jwt.verify(
       token,
-      process.env.JWT_SECRET_KEY || 'DoNotTellAnyOne'
+      process.env.JWT_SECRET_KEY || "DoNotTellAnyOne"
     );
 
     const user = await prisma.user.findUnique({
       where: { id: payload.userId },
     });
-    if (!user) return next(createError('Unauthenticated', 401));
+    if (!user) return next(createError("Unauthenticated", 401));
 
+    delete user.password;
     req.user = user;
     next();
   } catch (error) {
