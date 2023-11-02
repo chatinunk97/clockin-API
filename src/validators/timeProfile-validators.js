75ed02566ddc6bbc34037d16d2d2aa083fe2d0f8
Tar <Tarta12296@gmail.com>
2023-11-02 17:38:00 +0700

    FINISH Create,Update timeProfile

--- src/validators/timeProfile-validators.js
+++ src/validators/timeProfile-validators.js
@@ -3,11 +3,31 @@ const Joi = require("joi");
 const TypetimeEnum = ["DEFAULT", "FIRSTHALF", "SECONDHAFT", "NOTSPECIFIED"];
 
 const timeProfileSchema = Joi.object({
-  start: Joi.date().format("DD/MM/YYYY").utc().required(),
-  end: Joi.date().format("DD/MM/YYYY").utc().required(),
+  companyProfileId: Joi.number().integer().positive(),
+  start: Joi.date().required(),
+  end: Joi.date().required(),
   typeTime: Joi.string()
     .valid(...TypetimeEnum)
     .required(),
 });
+exports.timeProfileSchema = timeProfileSchema;
 
-module.exports = timeProfileSchema;
+// const getTimeProfileSchema = Joi.object({
+//   companyProfileId: Joi.number().integer().positive(),
+//   start: Joi.date().required(),
+//   end: Joi.date().required(),
+//   typeTime: Joi.string()
+//     .valid(...TypetimeEnum)
+//     .required(),
+// });
+// exports.getTimeProfileSchema = getTimeProfileSchema;
+
+const updateTimeProfileSchema = Joi.object({
+  // companyProfileId: Joi.number().integer().positive(),
+  start: Joi.date().required(),
+  end: Joi.date().required(),
+  typeTime: Joi.string()
+    .valid(...TypetimeEnum)
+    .required(),
+});
+exports.updateTimeProfileSchema = updateTimeProfileSchema;
