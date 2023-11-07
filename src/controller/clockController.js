b9845bcce60f707c906eb480bf345a8c7d5238cf
chatinun <chatinun.hobby@gmail.com>
2023-11-07 11:27:32 +0700

    Clock In check late time OK

--- src/controller/clockController.js
+++ src/controller/clockController.js
@@ -1,9 +1,9 @@
 const {
   clockInSchema,
   clockOutSchema,
-} = require('../validators/clock-validators');
-const prisma = require('../models/prisma');
-const createError = require('../utils/create-error');
+} = require("../validators/clock-validators");
+const prisma = require("../models/prisma");
+const createError = require("../utils/create-error");
 
 exports.clockIn = async (req, res, next) => {
   try {
@@ -11,12 +11,32 @@ exports.clockIn = async (req, res, next) => {
     if (error) {
       return next(error);
     }
+
+    //Get default timr
+    const result = await prisma.timeProfile.findFirst({
+      where: {
+        AND: [
+          { companyProfileId: req.user.companyProfileId },
+          { typeTime: "DEFAULT" },
+        ],
+      },
+    });
+    //Compare Time
+    const startTime = new Date(
+      value.clockInTime.split("T")[0] + " " + result.start
+    );
+    const clockInTime = new Date(value.clockInTime);
+
+    if (clockInTime > startTime) {
+      console.log(`You're late !`);
+      value.statusClockIn = "LATE";
+    }
+
     value.user = { connect: { id: req.user.id } };
     const clockIn = await prisma.clock.create({
       data: value,
     });
-    res.json({clockIn})
-    
+    res.json({ clockIn });
   } catch (error) {
     next(error);
   }
@@ -24,7 +44,7 @@ exports.clockIn = async (req, res, next) => {
 
 exports.clockOut = async (req, res, next) => {
   try {
-    res.json({message : "Reached Clock Out"})
+    res.json({ message: "Reached Clock Out" });
   } catch (error) {
     next(error);
   }
